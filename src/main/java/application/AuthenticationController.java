eba9c9b72e96932a2d17594351761710b5d0dcb6
So-Andrey <115418958+So-Andrey@users.noreply.github.com>
2023-05-18 22:53:49 +0300

    auth and register now is in only one window

--- src/main/java/application/AuthenticationController.java
+++ src/main/java/application/AuthenticationController.java
@@ -8,10 +8,17 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 import java.io.IOException;
 
 public class AuthenticationController {
+
+    private boolean registration = false;
+
+    @FXML
+    private Text label;
 
     @FXML
     private Button becomeAMemberButton;
@@ -28,35 +35,52 @@ public class AuthenticationController {
     @FXML
     void initialize() {
         becomeAMemberButton.setOnAction(event -> {
-            try {
-                Stage stage = new Stage();
-                stage.setTitle("Dragons collection manager");
-                stage.setScene(new Scene(new FXMLLoader(getClass().getResource("/fxml/registration.fxml")).load(), 1024, 720));
-                stage.setResizable(false);
-                stage.setMaximized(false);
-                stage.show();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+            if (registration) {
+                label.setText("authorisation");
+                logInButton.setText("Log In");
+                becomeAMemberButton.setText("Don't have an account? Sign Up");
+                registration = false;
+            } else {
+                label.setText("registration");
+                label.setTextAlignment(TextAlignment.CENTER);
+                logInButton.setText("Submit");
+                logInButton.setTextAlignment(TextAlignment.CENTER);
+                becomeAMemberButton.setText("Already registered? Log In");
+                registration = true;
             }
         });
         logInButton.setOnAction(event -> {
-            if (UserAuthentication.userLoggingIn(loginField.getText(), passwordField.getText())) {
-                logInButton.getScene().getWindow().hide();
-                DragonsCollection.putDragonsFromDB();
-                try {
-                    Stage stage = new Stage();
-                    stage.setTitle("Dragons collection manager");
-                    stage.setScene(new Scene(new FXMLLoader(getClass().getResource("/fxml/table.fxml")).load(), 1024, 720));
-                    stage.setResizable(false);
-                    stage.setMaximized(false);
-                    stage.show();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
+            if (registration) {
+                if (UserAuthentication.userRegistration(loginField.getText(), passwordField.getText())) {
+                    label.setText("authorisation");
+                    logInButton.setText("Log In");
+                    loginField.setText("");
+                    passwordField.setText("");
+                    registration = false;
+                } else {
+                    loginField.setText("");
+                    passwordField.setText("");
+                    loginField.setPromptText("Login already exists");
                 }
             } else {
-                loginField.setText("");
-                passwordField.setText("");
-                loginField.setPromptText("Invalid login or password");
+                if (UserAuthentication.userLoggingIn(loginField.getText(), passwordField.getText())) {
+                    logInButton.getScene().getWindow().hide();
+                    DragonsCollection.putDragonsFromDB();
+                    try {
+                        Stage stage = new Stage();
+                        stage.setTitle("Dragons collection manager");
+                        stage.setScene(new Scene(new FXMLLoader(getClass().getResource("/fxml/table.fxml")).load(), 1024, 720));
+                        stage.setResizable(false);
+                        stage.setMaximized(false);
+                        stage.show();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                } else {
+                    loginField.setText("");
+                    passwordField.setText("");
+                    loginField.setPromptText("Invalid login or password");
+                }
             }
         });
     }

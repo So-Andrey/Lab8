4e7ba2c1d7be75c8684af277319218c3a3422f30
So-Andrey <115418958+So-Andrey@users.noreply.github.com>
2023-05-25 21:44:07 +0300

    now useless classes deleted;

--- src/main/java/commands/Invoker.java
+++ src/main/java/commands/Invoker.java
@@ -9,9 +9,6 @@ public class Invoker {
     /** Коллекция, через которую осуществляется выполнение команд */
     private static final HashMap<String, Command> commandHashMap = new HashMap<>();
     static {
-        commandHashMap.put("help", new HelpCommand());
-        commandHashMap.put("info", new InfoCommand());
-        commandHashMap.put("show", new ShowCommand());
         commandHashMap.put("add", new AddCommand());
         commandHashMap.put("update", new UpdateCommand());
         commandHashMap.put("remove_by_id", new RemoveByIdCommand());
@@ -20,9 +17,7 @@ public class Invoker {
         commandHashMap.put("add_if_min", new AddIfMinCommand());
         commandHashMap.put("remove_greater", new RemoveGreaterCommand());
         commandHashMap.put("remove_lower", new RemoveLowerCommand());
-        commandHashMap.put("max_by_head", new MaxByHeadCommand());
         commandHashMap.put("count_by_head", new CountByHeadCommand());
-        commandHashMap.put("print_ascending", new PrintAscendingCommand());
     }
     public static String[] getSplit() {
         return split;
